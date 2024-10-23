12ecaf1c23566e452ae07b69b5c5128f2105f900
FFreitas <francisco.freitas.ff@gmail.com>
2024-10-23 18:35:30 +0100

    unit converter done

--- UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -19,15 +19,27 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
 
@@ -46,15 +58,72 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter(modifier: Modifier = Modifier) {
+
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var firstSelectExpanded by remember { mutableStateOf(false) }
+    var secondSelectExpanded by remember { mutableStateOf(false) }
+    var firstSelectValue by remember { mutableStateOf(ConverterOption.Meters) }
+    var secondSelectValue by remember { mutableStateOf(ConverterOption.Meters) }
+    var conversionFactor by remember { mutableDoubleStateOf(1.00) }
+    var secondConversionFactor by remember { mutableDoubleStateOf(1.00) }
+
+    val customTextStyle = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.hsl(0f, 0.75f, 0.75f)
+    )
+
+    fun processConversion() {
+        val inputDouble = inputValue.toDoubleOrNull() ?: 0.0
+        val result =
+            (inputDouble * conversionFactor * 100 / secondConversionFactor).roundToInt() / 100.0
+        outputValue = result.toString()
+    }
+
+    fun onFirstSelect(firstOption: ConverterOption) {
+        firstSelectValue = firstOption
+        firstSelectExpanded = false
+        conversionFactor = when (firstOption) {
+            ConverterOption.Centimeters -> 0.01
+            ConverterOption.Meters -> 1.0
+            ConverterOption.Feet -> 0.3048
+            ConverterOption.Millimeters -> 0.001
+        }
+        processConversion()
+    }
+
+    fun onSecondSelect(secondOption: ConverterOption) {
+        secondSelectValue = secondOption
+        secondSelectExpanded = false
+        secondConversionFactor = when (secondOption) {
+            ConverterOption.Centimeters -> 0.01
+            ConverterOption.Meters -> 1.0
+            ConverterOption.Feet -> 0.3048
+            ConverterOption.Millimeters -> 0.001
+        }
+        processConversion()
+    }
+
+
+
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Text(text = "Unit Converter", modifier = Modifier.padding(bottom = 16.dp))
+        Text(
+            text = "Unit Converter",
+            modifier = Modifier.padding(bottom = 32.dp),
+            style = MaterialTheme.typography.headlineLarge
+        )
 
-        OutlinedTextField(value = "", onValueChange = { onChangeValueTextView() })
+        OutlinedTextField(
+            value = inputValue,
+            label = { Text("Input Value") },
+            onValueChange = { inputValue = it; processConversion() })
 
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -62,35 +131,54 @@ fun UnitConverter(modifier: Modifier = Modifier) {
 
             Box {
 
-                //Button(onClick = { Toast.makeText(LocalContext.current, "Funny Toast !!!!", Toast.LENGTH_LONG).show() }) { Text("Click Me!") }
-                Button(onClick = { onClickSelectFirst() }) {
-                    Text("Select")
+                Button(onClick = { firstSelectExpanded = true }) {
+                    Text(firstSelectValue.name)
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Select Arrow Down")
                 }
 
-                DropdownMenu(expanded = false, onDismissRequest = { })  {
-                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = {  })
-                    DropdownMenuItem(text = { Text("Meters") }, onClick = {  })
-                    DropdownMenuItem(text = { Text("Feet") }, onClick = {  })
-                    DropdownMenuItem(text = { Text("Millimeters") }, onClick = {  })
+                DropdownMenu(
+                    expanded = firstSelectExpanded,
+                    onDismissRequest = { firstSelectExpanded = false }) {
+                    DropdownMenuItem(
+                        text = { Text(ConverterOption.Centimeters.name) },
+                        onClick = { onFirstSelect(ConverterOption.Centimeters) })
+                    DropdownMenuItem(
+                        text = { Text(ConverterOption.Meters.name) },
+                        onClick = { onFirstSelect(ConverterOption.Meters) })
+                    DropdownMenuItem(
+                        text = { Text(ConverterOption.Feet.name) },
+                        onClick = { onFirstSelect(ConverterOption.Feet) })
+                    DropdownMenuItem(
+                        text = { Text(ConverterOption.Millimeters.name) },
+                        onClick = { onFirstSelect(ConverterOption.Millimeters) })
                 }
 
             }
 
-            Spacer(modifier = Modifier.width(8.dp))
+            Spacer(modifier = Modifier.width(16.dp))
 
             Box {
 
-                Button(onClick = { onClickSelectSecond() }) {
-                    Text("Select")
+                Button(onClick = { secondSelectExpanded = true }) {
+                    Text(secondSelectValue.name)
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Select Arrow Down")
                 }
 
-                DropdownMenu(expanded = false, onDismissRequest = { })  {
-                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = {  })
-                    DropdownMenuItem(text = { Text("Meters") }, onClick = {  })
-                    DropdownMenuItem(text = { Text("Feet") }, onClick = {  })
-                    DropdownMenuItem(text = { Text("Millimeters") }, onClick = {  })
+                DropdownMenu(
+                    expanded = secondSelectExpanded,
+                    onDismissRequest = { secondSelectExpanded = false }) {
+                    DropdownMenuItem(
+                        text = { Text(ConverterOption.Centimeters.name) },
+                        onClick = { onSecondSelect(ConverterOption.Centimeters) })
+                    DropdownMenuItem(
+                        text = { Text(ConverterOption.Meters.name) },
+                        onClick = { onSecondSelect(ConverterOption.Meters) })
+                    DropdownMenuItem(
+                        text = { Text(ConverterOption.Feet.name) },
+                        onClick = { onSecondSelect(ConverterOption.Feet) })
+                    DropdownMenuItem(
+                        text = { Text(ConverterOption.Millimeters.name) },
+                        onClick = { onSecondSelect(ConverterOption.Millimeters) })
                 }
 
             }
@@ -98,26 +186,18 @@ fun UnitConverter(modifier: Modifier = Modifier) {
         }
 
         Spacer(modifier = Modifier.height(16.dp))
-        
-        Text(text = "Result: ")
+
+        Text(
+            text = "Result: $outputValue ${secondSelectValue.name}",
+            style = MaterialTheme.typography.headlineMedium
+        )
     }
-}
 
-fun onClickSelectFirst() {
-    TODO("Not yet implemented")
-}
-
-fun onClickSelectSecond() {
-    TODO("Not yet implemented")
-}
-
-fun onChangeValueTextView() {
-    TODO("Not yet implemented")
 }
 
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewUnitConverter() {
-    UnitConverterTheme {  UnitConverter()  }
+    UnitConverterTheme { UnitConverter() }
 }
