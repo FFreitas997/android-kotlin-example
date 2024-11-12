20e6f4147f4f65e6f77b90947326b2e42d4c7d2c
FFreitas <francisco.freitas.ff@gmail.com>
2024-11-12 15:33:03 +0000

    kotlin basics

--- Kotlin Basics/src/Inheritance﻿.kt
+++ Kotlin Basics/src/Inheritance﻿.kt
@@ -9,6 +9,32 @@
  * 4. **init block**: The `init` block is used to initialize properties of a class.
  * 5. **Abstract classes**: Abstract classes are used to define a common interface for subclasses. They cannot be instantiated.
  * 6. **Data classes**: Data classes are used to store data. They automatically generate `equals()`, `hashCode()`, and `toString()` methods. (DTOs)
+ *
+ * ### Visibility Modifiers
+ *
+ * 1. **Public**: The class, method, or property is accessible from anywhere.
+ * 2. **Protected**: The class, method, or property is accessible within the same package and subclasses.
+ * 3. **Private**: The class, method, or property is accessible within the same file.
+ * 4. **Internal**: The class, method, or property is accessible within the same module.
+ *
+ * ### Nested classes and Inner classes
+ *
+ * 1. **Nested classes**: A class declared inside another class is called a nested class. It cannot access the outer class members.
+ * 2. **Inner classes**: An inner class is a nested class that is marked with the `inner` keyword. It can access the outer class members.
+ *
+ * ### Sealed classes
+ *
+ * Sealed classes are used to represent restricted class hierarchies. They are useful when a value can have one of a fixed set of types.
+ *
+ * Sealed classes are useful:
+ *
+ *1. **Limited class inheritance is desired**: You have a predefined, finite set of subclasses that extend a class, all of which are known at compile time.
+ *2. **Type-safe design is required**: Safety and pattern matching are crucial in your project. Particularly for state management or handling complex conditional logic. For an example, check out Use sealed classes with when expressions.
+ *3. **Working with closed APIs**: You want robust and maintainable public APIs for libraries that ensure that third-party clients use the APIs as intended.
+ *
+ * ### Interfaces
+ *
+ * Interfaces are used to define a contract for classes. They can contain abstract methods, properties, and default implementations.
  */
 
 interface Contract {
@@ -57,6 +83,37 @@ class DerivedAbstractClass : AbstractClass() {
 }
 
 data class DataClass(val name: String)
+
+class OuterClass {
+
+    val outerProperty = "Outer property"
+
+    class NestedClass {
+
+        val innerProperty = "Inner property"
+
+        fun doSomething() {
+            println("Doing something in the nested class")
+        }
+    }
+}
+
+
+class OuterClass1 {
+
+    val outerProperty = "Outer property"
+
+    inner class NestedClass {
+
+        val innerProperty = "Inner property"
+
+        fun doSomething() {
+            println(outerProperty)
+            println("Doing something in the nested class")
+        }
+    }
+}
+
 
 fun main() {
 
